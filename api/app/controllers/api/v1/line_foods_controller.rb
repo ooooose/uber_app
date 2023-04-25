dadfe9516c845de8ba61b0cc747e7515f8d62b01
ooooose <yuuki.oose1990@gmail.com>
2023-04-25 09:19:31 +0900

    [Add]indexアクションの作成

--- api/app/controllers/api/v1/line_foods_controller.rb
+++ api/app/controllers/api/v1/line_foods_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::LineFoodsController < ApplicationController
   before_action :set_food, only: %i[create]
 
+  def index
+    line_foods = LineFood.active
+    if line_foods.exists?
+      render json: {
+        line_food_ids: line_foods.map{ |line_food| line_food.id },
+        restaurant: line_foods[0].restaurant,
+        count: ilne_foods.sum{ |line_food| line_food[:count] },
+        amount: line_foods.sum{ |line_food| line_food.total_amount },
+      }, status: :ok
+    else
+      render json: {}, status: :no_content
+    end
+  end
+
   def create
     if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
       return render json: {
