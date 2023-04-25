ea201cb6853eb729f1309dbfdc14122b84be10d2
ooooose <yuuki.oose1990@gmail.com>
2023-04-26 07:20:47 +0900

    [Add]fetchRestaurantsの設定

--- front/src/containers/Restaurants.tsx
+++ front/src/containers/Restaurants.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
+
 
 const Restaurants: React.FC = () => {
+    useEffect(() => {
+        fetchRestaurants()
+        .then((data) => console.log(data))
+    }, []);
     return (
         <>
             <h1>レストラン一覧!!</h1>
